032dea9c72aab8958d48e125643746dc90f46c08
Vitaly Ostapenko <Ostapenko.Vitaly@gmail.com>
2015-02-03 13:51:58 +0000

    Exercises Filling in the layout

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,15 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  test "should get new" do
+
+  def setup                                           
+    @base_title = "Ruby on Rails Tutorial Sample App" 
+  end                                                 
+
+  test "should get signup" do
     get :new
     assert_response :success
+    assert_select "title", "Sign up | #{@base_title}"
   end
 
 end
