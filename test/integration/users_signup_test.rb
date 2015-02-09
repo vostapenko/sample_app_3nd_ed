80f7de0a2f861737644f4135c3d4d27acc0c1679
Vitaly Ostapenko <Ostapenko.Vitaly@gmail.com>
2015-02-09 08:14:28 +0000

    Exercises Sign up

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,16 +11,19 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                  password_confirmation: "bar" }
      end
      assert_template 'users/new'
+     assert_select 'div#error_explanation'
+     assert_select 'div.field_with_errors'
   end
 
   test "valid signup information" do                           
     get signup_path                                              
       assert_difference 'User.count', 1 do                       
-        post_via_redirect users_path, user: { name:  "Example User",                      
+        post_via_redirect users_path, user: { name: "Example User",               
                                       email: "example@example.com",          
                                       password:              "password",   
                                       password_confirmation: "password" }  
      end                                                         
      assert_template 'users/show'                                 
+     assert_not flash.nil?
   end                                                            
 end
