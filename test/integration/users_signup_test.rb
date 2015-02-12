3d2bc637ebdc1de0dd2ef34f5a57399cd2214fd0
Vitaly Ostapenko <Ostapenko.Vitaly@gmail.com>
2015-02-12 09:41:36 +0000

    Finish log in/log out

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -17,13 +17,14 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 
   test "valid signup information" do                           
     get signup_path                                              
-      assert_difference 'User.count', 1 do                       
-        post_via_redirect users_path, user: { name: "Example User",               
-                                      email: "example@example.com",          
-                                      password:              "password",   
-                                      password_confirmation: "password" }  
+    assert_difference 'User.count', 1 do                       
+      post_via_redirect users_path, user: { name: "Example User",               
+                                    email: "example@example.com",          
+                                    password:              "password",   
+                                    password_confirmation: "password" }  
      end                                                         
      assert_template 'users/show'                                 
+     assert is_logged_in?
      assert_not flash.nil?
   end                                                            
 end
